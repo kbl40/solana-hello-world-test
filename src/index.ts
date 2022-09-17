c4240061c824e5bf5a101358155bcdb7ed0eca0a
Kyle B. Lackinger <67078189+kbl40@users.noreply.github.com>
2022-09-17 09:36:40 -0400

    logic to connect to program

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,44 @@
 import { initializeKeypair } from "./initializeKeypair"
 import * as web3 from "@solana/web3.js"
 
+async function sayHello(
+  connection: web3.Connection,
+  payer: web3.Keypair
+): Promise<web3.TransactionSignature> {
+  const transaction = new web3.Transaction()
+
+  const instruction = new web3.TransactionInstruction(
+    {
+      keys: [],
+      programId: new web3.PublicKey('Fyk7KDikabboHsen2ovSE6mt4hcXqAVk1GZXjdrRPRNu')
+    }
+  )
+
+  transaction.add(instruction)
+
+  const txSig = await web3.sendAndConfirmTransaction(
+    connection,
+    transaction,
+    [payer]
+  )
+
+  return txSig
+}
+
 async function main() {
   const connection = new web3.Connection(web3.clusterApiUrl("devnet"))
   const user = await initializeKeypair(connection)
 
   console.log("PublicKey:", user.publicKey.toBase58())
+
+  const txSig = await sayHello(
+    connection,
+    user
+  )
+
+  console.log(
+    `Transaction: https://explorer.solana.com/tx/${txSig}?cluster=devnet`
+  )
 }
 
 main()
